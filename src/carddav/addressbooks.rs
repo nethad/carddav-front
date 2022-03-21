216dccd6fafac3b3265fad9230fe940f25261bbb
Thomas Ritter <thomas.ritter@mocoapp.com>
2022-03-21 20:23:39 +0100

    parse carddav request, refactoring, tests

--- src/carddav/addressbooks.rs
+++ src/carddav/addressbooks.rs
@@ -6,30 +6,64 @@ const STATUS_OK: &str = "HTTP/1.1 200 OK";
 const XMLNS_D: (&str, &str) = ("xmlns:d", "DAV:");
 const XMLNS_CARD: (&str, &str) = ("xmlns:card", "urn:ietf:params:xml:ns:carddav");
 
-pub fn build_addressbooks_response() -> String {
+pub fn principal_path(identifier: &str) -> String {
+    format!("/carddav/principals/users/{}/", identifier)
+}
+
+pub struct PropRequest {
+    props: Vec<String>,
+}
+
+pub fn parse_carddav_request(xml_content: &str) -> PropRequest {
+    let doc = roxmltree::Document::parse(xml_content).unwrap();
+    let root_element = doc.root_element();
+    if root_element.tag_name().name() == "propfind" {
+        let prop = root_element.children().find(|n| n.has_tag_name("prop"));
+        match prop {
+            Some(node) => {
+                let props = node
+                    .children()
+                    .filter(|n| n.is_element())
+                    .map(|n| n.tag_name().name().to_string())
+                    .collect::<Vec<String>>();
+                PropRequest { props }
+            }
+            None => PropRequest { props: vec![] },
+        }
+    } else {
+        PropRequest { props: vec![] }
+    }
+}
+
+fn build_propstat_block(prop_request: PropRequest) -> XMLElement {
+    let prop_blocks = prop_request
+        .props
+        .iter()
+        .map(|prop| match prop.as_str() {
+            "current-user-principal" => Some(elem_with_children(
+                elem("d:current-user-principal"),
+                vec![text_node("d:href", &principal_path("test@example.org"))],
+            )),
+            _ => None,
+        })
+        .filter_map(std::convert::identity)
+        .collect::<Vec<_>>();
+
+    elem_with_children(
+        elem("d:propstat"),
+        vec![
+            elem_with_children(elem("d:prop"), prop_blocks),
+            text_node("d:status", STATUS_OK),
+        ],
+    )
+}
+
+pub fn build_carddav_response(prop_request: PropRequest) -> String {
     let root = elem_with_children(
         elem_with_attrs("d:multistatus", &[XMLNS_D, XMLNS_CARD]),
         vec![elem_with_children(
             elem("d:response"),
-            vec![
-                text_node("d:href", "/"),
-                elem_with_children(
-                    elem("d:propstat"),
-                    vec![
-                        elem_with_children(
-                            elem("d:prop"),
-                            vec![elem_with_children(
-                                elem("d:current-user-principal"),
-                                vec![text_node(
-                                    "d:href",
-                                    "/carddav/principals/users/test@example.org/",
-                                )],
-                            )],
-                        ),
-                        text_node("d:status", STATUS_OK),
-                    ],
-                ),
-            ],
+            vec![text_node("d:href", "/"), build_propstat_block(prop_request)],
         )],
     );
 
@@ -67,10 +101,12 @@ fn add_xmlns(elem: &mut XMLElement, xmlns: &(&str, &str)) {
 
 #[cfg(test)]
 mod tests {
+    use std::fs;
+
     use super::*;
 
     #[test]
-    fn test_build_addressbooks_response() {
+    fn test_build_carddav_response() {
         let expected = "<?xml version = \"1.0\" encoding = \"UTF-8\"?>\n\
       <d:multistatus xmlns:d=\"DAV:\" xmlns:card=\"urn:ietf:params:xml:ns:carddav\">\n\
       \t<d:response>\n\
@@ -85,6 +121,19 @@ mod tests {
       \t\t</d:propstat>\n\
       \t</d:response>\n\
       </d:multistatus>\n";
-        assert_eq!(expected, build_addressbooks_response())
+
+        let prop_request = PropRequest {
+            props: vec!["current-user-principal".to_string()],
+        };
+        assert_eq!(expected, build_carddav_response(prop_request))
+    }
+
+    #[test]
+    fn test_parse_carddav_request() {
+        let content = fs::read_to_string("tests/fixtures/request/current-user-principal.xml")
+            .expect("Read fixture failed");
+
+        let request = parse_carddav_request(&content);
+        assert_eq!(["current-user-principal"], request.props.as_slice())
     }
 }
